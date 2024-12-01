ab26f7304935d5e3146bf35dc599df350d93867c
unknown <devhorla0@gmail.com>
2024-12-01 02:17:17 -0800

    Major Update

--- ipgeolocate.py
+++ ipgeolocate.py
@@ -2,86 +2,112 @@ import geocoder
 import reverse_geocoder as rg
 import folium
 import argparse
+import socket
+import re
 
-banner = r"""
----------------------------------------------------------------------
-|-|---------------------------------------------------------------|-|
-(~|   ___ ____        ____            _                 _         |~)       
-|~)  |_ _|  _ \      / ___| ___  ___ | | ___   ___ __ _| |_ ___   (~|
-(~|   | || |_) |____| |  _ / _ \/ _ \| |/ _ \ / __/ _` | __/ _ \  |~)
-|~)   | ||  __/_____| |_| |  __/ (_) | | (_) | (_| (_| | ||  __/  (~|
-(~|  |___|_|         \____|\___|\___/|_|\___/ \___\__,_|\__\___|  |~)                                              
-|-|---------------------------------------------------------------|-| 
-(~|  Geographical location/address finder using ip address        |~)
-|-|---------------------------------------------------------------|-|
-|~)    Developed by: sys_br3ach3r                                 (~|
-|-|---------------------------------------------------------------|-|
-[~|                 A.K.A @Horlalaycon @github                    |~]
----------------------------------------------------------------------
-                            +---------+         -----
-                           +-----------+        |   |
-                          +-------------+       -----
-=====================================================================
-=====================================================================                
-"""
+
+# domain name option processing
+def convert_domain_name(domain):
+	try:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.settimeout(1)
+		ip = socket.gethostbyname(domain)
+		pattern= r"(\d+\.\d+\.\d+\.\d+)"
+		match = re.search(pattern, ip)
+		match_result = match.group(1)
+		return match_result
+
+	except socket.gaierror:
+		print(r"""
+========================================
+ Error!:
+    Incorrect domain name 
+            or
+    No internet connection
+========================================
+""")
+		quit()
+
 
 # arguments
-parser = argparse.ArgumentParser(prog="IP Geolocate", description="A program to get geographical location/find location information from an ip address ")
-parser.add_argument("-t", "--target", help="specify IP address to geo-locate", required=True)
+parser = argparse.ArgumentParser(description="A program to get geographical location/find location information from an ip address ")
+
+# One of the two options must be selected
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-ip", "--ip_addr", help="specify Target's IP address to geolocate", )
+group.add_argument("-d", "--domain", help="Specify Target's Domain name to geolocate")
+
 parser.add_argument("-m", "--map", help="specify map name to generate location map ")
 args = parser.parse_args()
 
-target = args.target
+# Target
+if args.domain:
+	target = convert_domain_name(args.domain)
+else:
+	target = args.ip_addr
 
 
 def get_latlng(ip_addr=target):
-    g = geocoder.ip(ip_addr)
-    latitude, longitude = g.latlng
-    return latitude, longitude
+	g = geocoder.ip(ip_addr)
+	latitude, longitude = g.latlng
+	return latitude, longitude
 
 
-# geolocate engine
+# geolocator engine
 def locate():
-    city = ""
-    region1 = ""
-    region2 = ""
-    country = ""
-    lat, lng = get_latlng(target)
-    ip_info = rg.search((lat, lng), verbose=False)
-    for item in ip_info:
-        city = item["name"]
-        region1 = item["admin1"]
-        region2 = item["admin2"]
-        country = item["cc"]
+	city = ""
+	region1 = ""
+	region2 = ""
+	country = ""
+	lat, lng = get_latlng(target)
+	ip_info = rg.search((lat, lng), verbose=False)
+	for item in ip_info:
+		city = item["name"]
+		region1 = item["admin1"]
+		region2 = item["admin2"]
+		country = item["cc"]
 
-    return str(f"""++++++++++++++++++++++++++++++++++
-    City: {city}
-    State: {region1}
-    Region: {region2}
-    Country: {country}
-+++++++++++++++++++++++++++++++++++++++++++++++++""")
+	return str(f"""----------------------------------------
+	City: {city}
+	State: {region1}
+	Region: {region2}
+	Country: {country}
+	Lat, Lng: {lat, lng}
+========================================
+""")
 
 
 if __name__ == "__main__":
-    print(banner)
-    try:
-        print(f"++++++++++++++++++++++++++++++++++\n Locating target: {target}")
-        print(locate())
-        # map
-        if args.map:
-            map_name = args.map + ".html"
-            print(f"""|| Creating and saving Map ({map_name})
-+++++++++++++++++++++++++++++++++++++++++++++++++""")
-            latlng = get_latlng()
-            save_map = folium.Map(location=latlng, zoom_start=10)
-            folium.CircleMarker(location=latlng, radius=100, popup="Found Location").add_to(save_map)
-            save_map.save(map_name)
-    # except TypeError:
-    #     print(f"Error, check network connectivity")
-    #
-    except KeyboardInterrupt:
-        print(f"""
+	# banner
+	print("=" * 40)
+	print("	 *****(IP GEOLOCATOR)*****")
+	print("-" * 40)
+
+	print("   By: sys_br3ach3r")
+	print("=" * 40)
+
+	try:
+		print(f"\n  Locating target: {target}")
+		print(locate())
+
+		# map
+		if args.map:
+			map_name = args.map + ".html"
+			print(f""" Generating Map...
+	Map Saved: ({map_name})
+========================================""")
+			latlng = get_latlng()
+			save_map = folium.Map(location=latlng, zoom_start=10)
+			folium.CircleMarker(location=latlng, radius=100, popup="Found Location").add_to(save_map)
+			save_map.save(map_name)
+
+	except TypeError:
+		print(f"Error, check network connectivity")
+
+
+	except KeyboardInterrupt:
+		print(f"""
 ----------------------------------
 || program Aborting... (Ctrl+c) ||
 ----------------------------------""")
-        quit()
+		quit()
